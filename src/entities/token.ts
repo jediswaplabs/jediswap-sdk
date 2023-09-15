2391ddd2aca9e1aa217c565b74990608fee69682
Vinay Singh <vinay@mudrex.com>
2023-09-15 22:53:31 +0530

    added typed mapping to weth

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -70,7 +70,7 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
   }
 }
 
-export const WETH = {
+export const WETH: { [chainId in starknetChainId]: Token } = {
   [StarknetChainId.TESTNET]: new Token(
     StarknetChainId.TESTNET,
     '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
